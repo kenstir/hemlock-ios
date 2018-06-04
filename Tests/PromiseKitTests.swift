4676514beec31ac4a1b266427abf2d073753a638
Ken Cox <kenstir@gmail.com>
2018-06-04 14:57:05 -0400

    First test with PMKAlamofire, ugly but working

---
+++ Tests/PromiseKitTests.swift
@@ -0,0 +1,73 @@
+//
+//  PromiseKitTests.swift
+//
+//  AlamoTests.swift
+//
+//  Copyright (C) 2018 Kenneth H. Cox
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import XCTest
+import PromiseKit
+import PMKAlamofire
+import Foundation
+@testable import Hemlock
+
+class PromiseKitTests: XCTestCase {
+    let gatewayEncoding = URLEncoding(arrayEncoding: .noBrackets, boolEncoding: .numeric)
+    
+    func showAlert(_ error: Error) {
+        debugPrint(error)
+    }
+    
+    func test_basicGet() {
+        let expectation = XCTestExpectation(description: "async response")
+        
+        let req = Alamofire.request("https://httpbin.org/get")
+        let promise = req.responseJSON()
+        debugPrint(promise)
+        let promise2 = promise.done { json in
+            print("json:     \(json)")
+        }
+        let p3 = promise2.catch { error in
+            print("error!!!")
+            self.showAlert(error)
+        }
+        p3.finally {
+            print("finally")
+            expectation.fulfill()
+        }
+        
+//            debugPrint(rsp)
+//            let (json, response) = arg
+//            print("json:     \(json)")
+//            print("response: \(response)")
+//        }.catch{ error in
+//            showAlert(error)
+//        }.finally {
+//            expectation.fulfill()
+//        }
+        //            .responseJSON().then { json, response in
+        //                print("json:     \(json)")
+        //                print("response: \(response)")
+        //            }.catch{ error in
+        //                showAlert(error)
+        //            }.finally {
+        //                expectation.fulfill()
+        //        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
+}
