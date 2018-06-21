e2a3e7deba6d45f09275dbf10b2dcd9281bdf0c9
Ken Cox <kenstir@gmail.com>
2018-06-20 22:47:22 -0400

    Use production catalog URL

--- Source/PINES/AppSettings.swift
+++ Source/PINES/AppSettings.swift
@@ -25,7 +25,7 @@ struct AppSettings {
     //MARK: - Consortium Properties
 
     static let appTitle = "PINES"
-    static let url = "https://kenstir.ddns.net"// "https://gapines.org"
+    static let url = "https://gapines.org"
     
     //MARK: - Theme Properties
 
