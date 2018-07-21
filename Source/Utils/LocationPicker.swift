f3574cc81a8d176632970b3e210a39713fe250b6
Ken Cox <kenstir@gmail.com>
2018-07-21 14:01:02 -0400

    Tweaked Copyright

--- Source/Utils/LocationPicker.swift
+++ Source/Utils/LocationPicker.swift
@@ -1,8 +1,7 @@
 //
-//  McPicker.swift
-//  Hemlock
+//  LocationPicker.swift
 //
-//  Copyright (C) 2018 Kenneth H. Cox
+//  Copyright (C) 2018 Erik Cox
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
