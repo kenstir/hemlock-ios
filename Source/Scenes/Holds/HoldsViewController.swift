64640a1fb7a4635090a9f050bd631eea7539b202
Ken Cox <kenstir@gmail.com>
2018-07-29 14:51:09 -0400

    Refactored fetchTargetInfo into more manageable chunks
    in preparation for adding a PCRUD promise to the chains.

--- Source/Scenes/Holds/HoldsViewController.swift
+++ Source/Scenes/Holds/HoldsViewController.swift
@@ -21,6 +21,7 @@ import Foundation
 import UIKit
 import PromiseKit
 import PMKAlamofire
+import os.log
 
 class HoldsViewController: UIViewController {
     
@@ -30,7 +31,8 @@ class HoldsViewController: UIViewController {
 
     var items: [HoldRecord] = []
     var didCompleteFetch = false
-    
+    let log = OSLog(subsystem: AppSettings.logSubsystem, category: "Holds")
+
     //MARK: - UIViewController
 
     override func viewDidLoad() {
@@ -62,91 +64,123 @@ class HoldsViewController: UIViewController {
         // fetch holds
         let req = Gateway.makeRequest(service: API.circ, method: API.holdsRetrieve, args: [authtoken, userid])
         req.gatewayArrayResponse().done { objects in
-            try self.fetchHoldDetails(holds: HoldRecord.makeArray(objects))
+            self.items = HoldRecord.makeArray(objects)
+            try self.fetchHoldDetails()
         }.catch { error in
             self.presentGatewayAlert(forError: error)
         }
     }
     
-    func fetchHoldDetails(holds: [HoldRecord]) throws {
+    func fetchHoldDetails() throws {
         guard let authtoken = App.account?.authtoken else {
             throw HemlockError.sessionExpired()
         }
         var promises: [Promise<Void>] = []
-        for hold in holds {
-            promises.append(try fetchTargetInfo(hold: hold, authtoken: authtoken))
+        for hold in self.items {
+            promises.append(try fetchTargetDetails(hold: hold, authtoken: authtoken))
             promises.append(try fetchQueueStats(hold: hold, authtoken: authtoken))
         }
-        print("xxx \(promises.count) promises made")
+        os_log("%d promises made", log: self.log, type: .info, promises.count)
 
         firstly {
             when(fulfilled: promises)
         }.done {
-            print("xxx \(promises.count) promises fulfilled")
-            self.updateItems(holds: holds)
+            os_log("%d promises done", log: self.log, type: .info, promises.count)
+            self.updateItems()
         }.catch { error in
             self.presentGatewayAlert(forError: error)
         }
     }
     
-    func fetchTargetInfo(hold: HoldRecord, authtoken: String) throws -> Promise<Void> {
-        guard let target = hold.target else {
+    func fetchTargetDetails(hold: HoldRecord, authtoken: String) throws -> Promise<Void> {
+        guard let holdTarget = hold.target,
+            let holdType = hold.holdType else {
             return Promise<Void>() //TODO: add analytics
         }
-        var req: Alamofire.DataRequest
-        if hold.holdType == "T" {
-            req = Gateway.makeRequest(service: API.search, method: API.recordModsRetrieve, args: [target])
-            let promise = req.gatewayObjectResponse().done { obj in
-                print("xxx \(String(describing: hold.target)) fetchTargetInfo done")
-                hold.mvrObj = obj
-            }
-            return promise
+        if holdType == "T" {
+            return fetchTitleHoldTargetDetails(hold: hold, holdTarget: holdTarget, authtoken: authtoken)
         } else if hold.holdType == "M" {
-            req = Gateway.makeRequest(service: API.search, method: API.metarecordModsRetrieve, args: [target])
-            let promise = req.gatewayObjectResponse().done { obj in
-                print("xxx \(String(describing: hold.target)) fetchTargetInfo done")
-                hold.mvrObj = obj
-            }
-            return promise
+            return fetchMetarecordHoldTargetDetails(hold: hold, holdTarget: holdTarget, authtoken: authtoken)
         } else if hold.holdType == "P" {
-            var param: [String: Any] = [:]
-            param["cache"] = 1
-            param["fields"] = ["label", "record"]
-            param["query"] = ["id": target]
-            req = Gateway.makeRequest(service: API.fielder, method: API.fielderBMPAtomic, args: [param])
-            let promise = req.gatewayArrayResponse().then { (array: [OSRFObject]) -> Promise<(OSRFObject)> in
-                var target = 0
-                if let obj = array.first {
-                    target = obj.getInt("record") ?? 0
-                    hold.label = obj.getString("label")
-                }
-                return Gateway.makeRequest(service: API.search, method: API.recordModsRetrieve, args: [target]).gatewayObjectResponse()
-            }.done { (obj: OSRFObject) -> Void in
-                print("xxx \(String(describing: hold.target)) fetchTargetInfo done")
-                hold.mvrObj = obj
-            }
-            return promise
+            return fetchPartHoldTargetDetails(hold: hold, holdTarget: holdTarget, authtoken: authtoken)
         } else {
+            os_log("fetchTargetInfo target=%d holdType=%@ NOT HANDLED", log: log, type: .info, holdTarget, holdType)
             return Promise<Void>() //TODO: add analytics
         }
+    }
+
+    func fetchTitleHoldTargetDetails(hold: HoldRecord, holdTarget: Int, authtoken: String) -> Promise<Void> {
+        os_log("fetchTargetInfo target=%d holdType=T mods start", log: self.log, type: .info, holdTarget)
+        let req = Gateway.makeRequest(service: API.search, method: API.recordModsRetrieve, args: [holdTarget])
+        let promise = req.gatewayObjectResponse().done { obj in
+            os_log("fetchTargetInfo target=%d holdType=T mods done", log: self.log, type: .info, holdTarget)
+            hold.metabibRecord = MBRecord(id: holdTarget, mvrObj: obj)
+        }
+        return promise
+    }
+
+    func fetchMetarecordHoldTargetDetails(hold: HoldRecord, holdTarget: Int, authtoken: String) -> Promise<Void> {
+        os_log("fetchTargetInfo target=%d holdType=M mods start", log: self.log, type: .info, holdTarget)
+        let req = Gateway.makeRequest(service: API.search, method: API.metarecordModsRetrieve, args: [holdTarget])
+        let promise = req.gatewayObjectResponse().done { obj in
+            os_log("fetchTargetInfo target=%d holdType=M mods done", log: self.log, type: .info, holdTarget)
+            hold.metabibRecord = MBRecord(id: holdTarget, mvrObj: obj)
+        }
+        return promise
+    }
+
+    func fetchPartHoldTargetDetails(hold: HoldRecord, holdTarget: Int, authtoken: String) -> Promise<Void> {
+        os_log("fetchTargetInfo target=%d holdType=P fielder start", log: self.log, type: .info, holdTarget)
+            var param: [String: Any] = [:]
+        param["cache"] = 1
+        param["fields"] = ["label", "record"]
+        param["query"] = ["id": holdTarget]
+        let req = Gateway.makeRequest(service: API.fielder, method: API.fielderBMPAtomic, args: [param])
+        let promise = req.gatewayArrayResponse().then { (array: [OSRFObject]) -> Promise<(OSRFObject)> in
+            var target = 0
+            if let obj = array.first {
+                target = obj.getInt("record") ?? 0
+                hold.label = obj.getString("label")
+            }
+            os_log("fetchTargetInfo target=%d holdType=P targetRecord=%d fielder done mods start", log: self.log, type: .info, holdTarget, target)
+            return Gateway.makeRequest(service: API.search, method: API.recordModsRetrieve, args: [target]).gatewayObjectResponse()
+        }.done { (obj: OSRFObject) -> Void in
+            os_log("fetchTargetInfo target=%d holdType=P mods done", log: self.log, type: .info, holdTarget)
+            if let id = obj.getInt("doc_id") {
+                hold.metabibRecord = MBRecord(id: id, mvrObj: obj)
+            }
+        }
+        return promise
     }
     
     func fetchQueueStats(hold: HoldRecord, authtoken: String) throws -> Promise<Void> {
         guard let id = hold.ahrObj.getID("id") else {
             return Promise<Void>() //TODO: add analytics
         }
+        let holdTarget = hold.target ?? 0
+        os_log("fetchQueueStats target=%d start", log: self.log, type: .info, holdTarget)
         let req = Gateway.makeRequest(service: API.circ, method: API.holdQueueStats, args: [authtoken, id])
         let promise = req.gatewayObjectResponse().done { obj in
-            print("xxx \(String(describing: hold.target)) queueStats done")
+            os_log("fetchQueueStats target=%d done", log: self.log, type: .info, holdTarget)
             hold.qstatsObj = obj
         }
         return promise
     }
 
-    func updateItems(holds: [HoldRecord]) {
-        self.items = holds
+    // this is duplicated in XResultsViewController
+    func fetchSearchFormat(authtoken: String, forRecord record: MBRecord) -> Promise<Void> {
+        os_log("fetchSearchFormat id=%d start", log: self.log, type: .info, record.id)
+        let req = Gateway.makeRequest(service: API.pcrud, method: API.retrieveMRA, args: [API.anonymousAuthToken, record.id])
+        let promise = req.gatewayObjectResponse().done { obj in
+            os_log("fetchSearchFormat id=%d done", log: self.log, type: .info, record.id)
+            record.searchFormat = Format.getSearchFormat(fromMRAObject: obj)
+        }
+        return promise
+    }
+
+    func updateItems() {
         self.didCompleteFetch = true
-        print("xxx \(items.count) records now, time to reloadData")
+        os_log("updateItems %d items", log: self.log, type: .info, items.count)
         holdsTable.reloadData()
     }
 }
