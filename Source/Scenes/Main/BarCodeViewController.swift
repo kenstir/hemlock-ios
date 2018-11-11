b112940daaf190e4331edaa8af821941345e55fe
Ken Cox <kenstir@gmail.com>
2018-11-11 14:52:47 -0500

    Load real barcode from network
    Also fixed up copyright.

--- Source/Scenes/Main/BarCodeViewController.swift
+++ Source/Scenes/Main/BarCodeViewController.swift
@@ -1,13 +1,24 @@
 //
 //  BarCodeViewController.swift
-//  Hemlock
 //
-//  Created by Erik Cox on 11/9/18.
-//  Copyright © 2018 Ken Cox. All rights reserved.
+//  Copyright (C) 2018 Erik Cox
 //
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-import Foundation
 import UIKit
+import PromiseKit
 import ZXingObjC
 
 class BarCodeViewController: UIViewController {
@@ -26,19 +37,47 @@ class BarCodeViewController: UIViewController {
     }
     
     @IBOutlet weak var barCodeImage: UIImageView!
+    var didCompleteFetch = false
+
     //MARK: - UIViewController
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !didCompleteFetch {
+            fetchData()
+        }
+    }
+    
+    //MARK: - Functions
+    
     func setupViews() {
         let writer = ZXMultiFormatWriter()
-        if let matrix = try? writer.encode("1234567", format: kBarcodeFormatCodabar, width: 500, height: 200) {
-            debugPrint(matrix)
-            if let cgimage = ZXImage(matrix: matrix).cgimage {
-                barCodeImage.image = UIImage(cgImage: cgimage)
+        if let matrix = try? writer.encode("1234567", format: kBarcodeFormatCodabar, width: 400, height: 200),
+            let cgimage = ZXImage(matrix: matrix).cgimage
+        {
+            barCodeImage.image = UIImage(cgImage: cgimage)
+        }
+    }
+
+    func fetchData() {
+        guard let account = App.account else
+        {
+            presentGatewayAlert(forError: HemlockError.sessionExpired())
+            return //TODO: add analytics
+        }
+
+        let promise = ActorService.fetchUserSettings(account: account)
+        promise.done {
+            if let barcode = account.barcode {
+                print("barcode: \(barcode)")
             }
+        }.catch { error in
+            self.presentGatewayAlert(forError: error)
         }
     }
 }
