0cc81ecf9f004f42fcd824fc0bb4f7698b388f9e
Ken Cox <kenstir@gmail.com>
2018-12-23 16:58:26 -0500

    Skeleton COOL app

---
+++ Source/cool_app/CoolTheme.swift
@@ -0,0 +1,36 @@
+//
+//  Theme.swift
+//
+//  Copyright (C) 2018 Kenneth H. Cox
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import UIKit
+
+class CoolTheme: Theme {
+    let foregroundColor = UIColor.white
+    let backgroundColor = UIColor(red: 0x37/0xff, green: 0x96/0xff, blue: 0x76/0xff, alpha: 1.0)
+    let backgroundDark1 = UIColor(red: 0x32/0xff, green: 0x89/0xff, blue: 0x6c/0xff, alpha: 1.0)
+    let backgroundDark2 = UIColor(red: 0x2e/0xff, green: 0x7b/0xff, blue: 0x61/0xff, alpha: 1.0)
+    let backgroundDark3 = UIColor(red: 0x29/0xff, green: 0x6e/0xff, blue: 0x56/0xff, alpha: 1.0)
+    let backgroundDark4 = UIColor(red: 0x23/0xff, green: 0x60/0xff, blue: 0x4c/0xff, alpha: 1.0)
+    let backgroundDark5 = UIColor(red: 0x1e/0xff, green: 0x52/0xff, blue: 0x41/0xff, alpha: 1.0)
+    let backgroundDark6 = UIColor(red: 0x19/0xff, green: 0x45/0xff, blue: 0x36/0xff, alpha: 1.0)
+    let backgroundDark7 = UIColor(red: 0x14/0xff, green: 0x37/0xff, blue: 0x2b/0xff, alpha: 1.0)
+    let backgroundDark8 = UIColor(red: 0x0f/0xff, green: 0x29/0xff, blue: 0x21/0xff, alpha: 1.0)
+
+    let tableHeaderForeground = UIColor.darkGray
+    let tableHeaderBackground = UIColor.groupTableViewBackground
+}
