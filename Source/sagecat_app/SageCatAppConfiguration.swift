fc474b4ced04c234535bad66e8b1418d1739e8fb
kenstir <kenstir@gmail.com>
2019-12-29 12:54:52 -0500

    WIP: sagecat

---
+++ Source/sagecat_app/SageCatAppConfiguration.swift
@@ -0,0 +1,39 @@
+//
+//  SageCatAppConfiguration.swift
+//
+//  Copyright (C) 2019 Kenneth H. Cox
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+class SageCatAppConfiguration: AppConfiguration {
+    let title = "SageCat Catalog"
+    let url = "https://catalog.northtexaslibraries.org"
+    let bugReportEmailAddress = "kenstir.apps@gmail.com"
+    var sort: String? = nil
+
+    let enableHierarchicalOrgTree = true
+    let enableHoldShowQueuePosition = true
+    let enableHoldPhoneNotification = false
+    let enableMainSceneBottomToolbar = false
+    let enablePayFines = false
+    let groupCopyInfoBySystem = false
+    let enableCopyInfoWebLinks = true
+    let needMARCRecord = false
+    let showOnlineAccessHostname = true
+    let alwaysPopupOnlineLinks = false
+
+    let barcodeFormat: BarcodeFormat = .Codabar
+    let searchLimit = 100
+}
