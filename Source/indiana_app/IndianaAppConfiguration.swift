875c5324f2d6915da7079034c77e7a4303c32f44
kenstir <kenstir@gmail.com>
2020-05-22 16:57:24 -0400

    Create skeleton app for Evergreen Indiana
    Instructions at howto_new_ios_target.md

---
+++ Source/indiana_app/IndianaAppConfiguration.swift
@@ -0,0 +1,40 @@
+//
+//  SageCatAppConfiguration.swift
+//
+//  Copyright (C) 2019 Kenneth H. Cox
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+class IndianaAppConfiguration: AppConfiguration {
+    let title = "Evergreen Indiana"
+    let url = "https://evergreen.lib.in.us"
+    let bugReportEmailAddress = "kenstir.apps@gmail.com"
+    var sort: String? = nil
+
+    let enableHierarchicalOrgTree = true
+    let enableHoldShowQueuePosition = true
+    let enableHoldPhoneNotification = true
+    let enableMainSceneBottomToolbar = false
+    let enablePayFines = true // TODO: verify with Anna
+    let groupCopyInfoBySystem = false
+    let enableCopyInfoWebLinks = false
+    let needMARCRecord = true
+    let showOnlineAccessHostname = false
+    let alwaysPopupOnlineLinks = true
+    let haveColorButtonImages = false
+
+    let barcodeFormat: BarcodeFormat = .Codabar
+    let searchLimit = 100
+}
