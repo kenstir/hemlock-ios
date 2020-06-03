4ffd02914d5efac29d357dd80710f77f8f003201
kenstir <kenstir@gmail.com>
2020-06-02 22:24:46 -0400

    Load hours of operation
    The display is terrible but I'll fix that.

--- Source/Scenes/OrgDetails/OrgDetailsViewController.swift
+++ Source/Scenes/OrgDetails/OrgDetailsViewController.swift
@@ -26,10 +26,17 @@ class OrgDetailsViewController: UIViewController {
     //MARK: - Properties
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var day0Hours: UILabel!
+    @IBOutlet weak var day1Hours: UILabel!
+    @IBOutlet weak var day2Hours: UILabel!
+    @IBOutlet weak var day3Hours: UILabel!
+    @IBOutlet weak var day4Hours: UILabel!
+    @IBOutlet weak var day5Hours: UILabel!
+    @IBOutlet weak var day6Hours: UILabel!
     
     weak var activityIndicator: UIActivityIndicatorView!
     
-    var org: Organization? = nil
+    var orgID: Int? = App.account?.homeOrgID
 
     var orgLabels: [String] = []
     var didCompleteFetch = false
@@ -44,9 +51,9 @@ class OrgDetailsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        if !didCompleteFetch {
+        //if !didCompleteFetch {
             fetchData()
-        }
+        //}
     }
     
     //MARK: - Functions
@@ -54,22 +61,37 @@ class OrgDetailsViewController: UIViewController {
     func fetchData() {
         var promises: [Promise<Void>] = []
         promises.append(ActorService.fetchOrgTypes())
-        promises.append(ActorService.fetchOrgTree())
+        promises.append(ActorService.fetchOrgTreeAndSettings(forOrgID: orgID))
         
         centerSubview(activityIndicator)
         self.activityIndicator.startAnimating()
-        
+
         firstly {
             when(fulfilled: promises)
         }.done {
-            self.didCompleteFetch = true
-        }.ensure {
-            self.activityIndicator.stopAnimating()
+            //self.didCompleteFetch = true
+            self.fetchHours()
+            self.onOrgsLoaded()
         }.catch { error in
+            self.activityIndicator.stopAnimating()
             self.presentGatewayAlert(forError: error)
         }
     }
     
+    func fetchHours() {
+        guard let authtoken = App.account?.authtoken,
+            let orgID = self.orgID else { return }
+        
+        ActorService.fetchOrgUnitHours(authtoken: authtoken, forOrgID: orgID).done { obj in
+            self.onHoursLoaded(obj)
+        }.ensure {
+            self.activityIndicator.stopAnimating()
+        }.catch { error in
+            self.activityIndicator.stopAnimating()
+            self.presentGatewayAlert(forError: error)
+        }
+    }
+
     func setupViews() {
         tableView.dataSource = self
         tableView.delegate = self
@@ -81,6 +103,40 @@ class OrgDetailsViewController: UIViewController {
     func setupActivityIndicator() {
         activityIndicator = addActivityIndicator()
         Style.styleActivityIndicator(activityIndicator)
+    }
+    
+    func onHoursLoaded(_ obj: OSRFObject?) {
+        if let open = obj?.getString("dow_0_open"), let close = obj?.getString("dow_0_close") { day0Hours.text = "\(open) - \(close)" }
+        if let open = obj?.getString("dow_1_open"), let close = obj?.getString("dow_1_close") { day1Hours.text = "\(open) - \(close)" }
+        if let open = obj?.getString("dow_2_open"), let close = obj?.getString("dow_2_close") { day2Hours.text = "\(open) - \(close)" }
+        if let open = obj?.getString("dow_3_open"), let close = obj?.getString("dow_3_close") { day3Hours.text = "\(open) - \(close)" }
+        if let open = obj?.getString("dow_4_open"), let close = obj?.getString("dow_4_close") { day4Hours.text = "\(open) - \(close)" }
+        if let open = obj?.getString("dow_5_open"), let close = obj?.getString("dow_5_close") { day5Hours.text = "\(open) - \(close)" }
+        if let open = obj?.getString("dow_6_open"), let close = obj?.getString("dow_6_close") { day6Hours.text = "\(open) - \(close)" }
+    }
+    
+    // init that can't happen until fetchData completes
+    func onOrgsLoaded() {
+        tableView.reloadData()
+        loadOrgData()
+    }
+
+    func loadOrgData() {
+        orgLabels = Organization.getSpinnerLabels()
+/*
+        var selectOrgIndex = 0
+        let defaultOrgID = App.account?.homeOrgID
+        for index in 0..<Organization.visibleOrgs.count {
+            let org = Organization.visibleOrgs[index]
+            if org.id == defaultOrgID {
+                selectOrgIndex = index
+            }
+        }
+        
+        selectedOrgName = orgLabels[selectOrgIndex].trim()
+        self.org = Organization.find(byName: selectedOrgName)
+        setupTitle()
+ */
     }
 }
 
@@ -99,8 +155,9 @@ extension OrgDetailsViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "orgChooserCell", for: indexPath)
         //cell.textLabel?.text = "Location"
         //cell.detailTextLabel?.text = org?.name
-        cell.textLabel?.text = org?.name ?? "Athens-Clarke County Library"
-        cell.detailTextLabel?.text = "details, details"
+        let org = Organization.find(byId: orgID)
+        cell.textLabel?.text = org?.name
+        //cell.detailTextLabel?.text = ""
         return cell
     }
 }
