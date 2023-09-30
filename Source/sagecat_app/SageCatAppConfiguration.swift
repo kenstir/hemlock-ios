f72c4af6a4051f5aebcf35baaa64ff6964861c8a
kenstir <kenstir@gmail.com>
2023-09-30 15:56:20 -0400

    sagecat: Update to latest configuration

--- Source/sagecat_app/SageCatAppConfiguration.swift
+++ Source/sagecat_app/SageCatAppConfiguration.swift
@@ -30,6 +30,7 @@ class SageCatAppConfiguration: AppConfiguration {
     let enableHoldShowQueuePosition = true
     let enableHoldShowExpiration = false
     let enableHoldPhoneNotification = true
+    let enableHoldUseOverride = false
     let enablePartHolds = true
     let enableTitleHoldOnItemWithParts = false
     let enableMainSceneBottomToolbar = false
