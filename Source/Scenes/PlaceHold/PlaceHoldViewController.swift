51c43de0e7a7ffcd132e3df772743c9d3352c33b
kenstir <kenstir@gmail.com>
2024-03-02 15:30:46 -0500

    Copy details storyboard to new place hold (#47)
    Try new tack: duplicate details storyboard
    because it already has the scroll region figured out
    and I fought enough with autolayout already.
    
    * New storyboard working and layout looks good
    * Add form label constraints at runtime
      Use width>=widestLabelWidth+8 for all visible form labels
      because using all equalWidth constraints did not work
      but resulted in the labels being too small and ellipsized.
    * Compact date pickers need .contentHorizontalAlignment = .left
    * UIView+disclosure made to work on iOS 13+ using chevron image

--- Source/Scenes/PlaceHold/PlaceHoldViewController.swift
+++ Source/Scenes/PlaceHold/PlaceHoldViewController.swift
@@ -23,11 +23,24 @@ class PlaceHoldViewController: UIViewController {
 
     //MARK: - Properties
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var partSelectStack: UIStackView!
     @IBOutlet weak var phoneNotifyStack: UIStackView!
     @IBOutlet weak var expirationStack: UIStackView!
     @IBOutlet weak var suspendStack: UIStackView!
     @IBOutlet weak var thawStack: UIStackView!
+
+    @IBOutlet weak var partTextField: UITextField!
+    @IBOutlet weak var pickupTextField: UITextField!
+    @IBOutlet weak var pickupButton: UIButton!
+    @IBOutlet weak var carrierTextField: UITextField!
+    @IBOutlet weak var expirationDatePicker: UIDatePicker!
+    @IBOutlet weak var thawDatePicker: UIDatePicker!
+
+    @IBOutlet weak var emailSwitch: UISwitch!
+    @IBOutlet weak var phoneSwitch: UISwitch!
+    @IBOutlet weak var smsSwitch: UISwitch!
+    @IBOutlet weak var suspendSwitch: UISwitch!
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
@@ -50,7 +63,7 @@ class PlaceHoldViewController: UIViewController {
     //MARK: - Lifecycle
 
     static func make(record: MBRecord, holdRecord: HoldRecord? = nil, valueChangedHandler: (() -> Void)? = nil) -> PlaceHoldViewController? {
-        if let vc = UIStoryboard(name: "PlaceHold", bundle: nil).instantiateInitialViewController() as? PlaceHoldViewController {
+        if let vc = UIStoryboard(name: "NewPlaceHold", bundle: nil).instantiateInitialViewController() as? PlaceHoldViewController {
             vc.record = record
             vc.holdRecord = holdRecord
             vc.valueChangedHandler = valueChangedHandler
@@ -62,8 +75,21 @@ class PlaceHoldViewController: UIViewController {
     //MARK: - UIViewController
 
     override func viewDidLoad() {
+        print("=============================== viewDidLoad")
         super.viewDidLoad()
+        for label in labels {
+            print("label: \(label.text ?? "") \(label.frame.width)")
+        }
         setupViews()
+    }
+
+    override func viewDidLayoutSubviews() {
+        print("=============================== viewDidLayoutSubviews")
+        for label in labels {
+            print("label: \(label.text ?? "") \(label.frame.width)")
+        }
+        print("expirePicker: \(expirationDatePicker.frame.width)")
+        print("thawPicker:   \(thawDatePicker.frame.width)")
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -75,8 +101,9 @@ class PlaceHoldViewController: UIViewController {
 
     func setupViews() {
         setupMetadataLabels()
-
         setupFormLabels()
+        setupTextViews()
+        setupPickers()
 
         actionButton.setTitle(isEditHold ? "Update Hold" : "Place Hold", for: .normal)
         actionButton.addTarget(self, action: #selector(holdButtonPressed(sender:)), for: .touchUpInside)
@@ -90,10 +117,50 @@ class PlaceHoldViewController: UIViewController {
     }
 
     func setupFormLabels() {
+        partSelectStack.isHidden = true
+        phoneNotifyStack.isHidden = true
+
+        // find the widest label whose superview (Hstack) is visible
+        guard let widestLabel = labels.max(by: { ($1.superview?.isHidden == false) && $1.frame.width > $0.frame.width }) else { return }
+        print("widest: \(widestLabel.text ?? "") \(widestLabel.frame.width)")
+
+        // Set a minimum width constraint to *slightly bigger* than the widest label wants to be.
+        // Setting an equalTo constraint resulted in all the labels being too thin and ellipsized.
+        let minWidth = widestLabel.frame.width + 8.0
         for label in labels {
-            print("label: \(label.text ?? "")")
+            print("label: \(label.text ?? "") new min width \(minWidth)")
+            label.widthAnchor.constraint(greaterThanOrEqualToConstant: minWidth).isActive = true
         }
-        print("stop here")
+    }
+
+    var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        if #available(iOS 13.0, *) {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 13, weight: .medium)
+            imageView.image = UIImage(systemName: "chevron.right", withConfiguration: configuration)
+            imageView.tintColor = .lightGray
+            imageView.contentMode = .scaleAspectFit
+            NSLayoutConstraint(item: imageView,
+                                         attribute: .height,
+                                         relatedBy: .equal,
+                                         toItem: imageView,
+                                         attribute: .width,
+                               multiplier: 17.0/10.0,
+                                         constant: 0).isActive = true
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+        }
+        return imageView
+    }()
+
+    func setupTextViews() {
+        partTextField.addDisclosureIndicator()
+        pickupTextField.addDisclosureIndicator()
+        carrierTextField.addDisclosureIndicator()
+    }
+
+    func setupPickers() {
+        expirationDatePicker.contentHorizontalAlignment = .left
+        thawDatePicker.contentHorizontalAlignment = .left
     }
 
     //MARK: - Functions
