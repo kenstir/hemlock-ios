f00f0f1ea9e6064abf53074f2b52b935fe9b555c
kenstir <kenstir@gmail.com>
2024-08-29 13:47:40 -0400

    Add working tag_release.sh script

--- tag_release.sh
+++ tag_release.sh
@@ -10,43 +10,69 @@ proj="Hemlock.xcodeproj/project.pbxproj"
 
 set -e
 
-## find the plist identifier of XCBuildConfiguration section for Debug and Release
+## find the plist identifier of the XCBuildConfiguration Release section for the given app
 
-cat "$proj" \
+buildver=$(cat "$proj" \
     | sed -ne '/Begin XCBuildConfiguration section/,/End XCBuildConfiguration section/ p' \
     | awk '
-    # Store the identifier
+    BEGIN {
+        app_matches = 0
+    }
+
+    # Store the identifier of Debug or Release blocks
     /[A-F0-9]+ \/\* (Debug|Release) \*\// {
         IDENT = $1
+        #print NR, $0
     }
 
-    # Print the stored identifier
+    # If the app matches, we are in the right block
     /CODE_SIGN_ENTITLEMENTS = Source\/'$app'_app/ {
-        print IDENT
-        exit
+        #print NR, $0
+        #print IDENT
+        app_matches = 1
     }
-'
-exit 1
 
+    # Get out at the end of the match block
+    /};/ {
+        if (app_matches) {
+           #print NR, $0
+           exit
+        }
+    }
 
-versionCode=$(egrep android:versionCode $manifest)
-versionCode=${versionCode#*\"}
-versionCode=${versionCode%\"*}
-echo versionCode=$versionCode
-test -n "$versionCode"
+    # Extract BUILD (includes semicolon)
+    /CURRENT_PROJECT_VERSION = / {
+        if (app_matches) {
+            BUILD = $3
+        }
+    }
 
-versionName=$(egrep android:versionName $manifest)
-versionName=${versionName#*\"}
-versionName=${versionName%\"*}
-echo versionName=$versionName
-test -n "$versionName"
+    # Extract VERSION (includes semicolon)
+    /MARKETING_VERSION = / {
+        if (app_matches) {
+            VERSION = $3
+        }
+    }
+
+    END {
+        print "BUILD=" BUILD
+        print "VERSION=" VERSION
+    }
+')
+
+echo "$buildver"
+eval "$buildver"
+test -n "$BUILD"
+test -n "$VERSION"
+
+## Construct a tag and tag it
 
 set -ex
 
-tag=${app}_v${versionCode}
-msg="${tag} (${versionName})"
+tag=${app}_${VERSION}.${BUILD}
+msg="${tag}"
 
-git commit core/build.gradle "$manifest" -m "$msg" || true
+git commit "$proj" -m "$msg" || true
 git tag -a -m "$msg" $tag
 git push
 #git push --tags
