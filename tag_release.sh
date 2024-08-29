bd779bd634718e1064bb8247a1bd6add18fb1e89
kenstir <kenstir@gmail.com>
2024-08-29 11:54:34 -0400

    WIP script to tag release

---
+++ tag_release.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 app_name"
+    exit 1
+fi
+app="$1"
+
+proj=""
+
+manifest="${app}_app/src/main/AndroidManifest.xml"
+if [ ! -f "$manifest" ]; then
+    echo "No such file: $manifest"
+    exit 1
+fi
+
+set -e
+
+versionCode=$(egrep android:versionCode $manifest)
+versionCode=${versionCode#*\"}
+versionCode=${versionCode%\"*}
+echo versionCode=$versionCode
+test -n "$versionCode"
+
+versionName=$(egrep android:versionName $manifest)
+versionName=${versionName#*\"}
+versionName=${versionName%\"*}
+echo versionName=$versionName
+test -n "$versionName"
+
+set -ex
+
+tag=${app}_v${versionCode}
+msg="${tag} (${versionName})"
+
+git commit core/build.gradle "$manifest" -m "$msg" || true
+git tag -a -m "$msg" $tag
+git push
+#git push --tags
+git push origin $tag
