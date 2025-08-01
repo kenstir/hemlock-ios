976bab0f88694807c33c6579c00ea567ddf2c6c9
kenstir <kenstir@gmail.com>
2025-08-01 13:56:47 -0400

    Fix deprecation warnings due to min iOS bump
    * Removed .contentEdgeInsets that were not working anyway
    * Removed unneeded old md5 algo
    * Fixed UIFont.withSmallCaps to use updated feature keys
    * Swapped UIActivityIndicatorView style for new name
    * Adapt updated guidance for finding keyWindow

--- Source/Utils/MD5.swift
+++ Source/Utils/MD5.swift
@@ -1,22 +1,3 @@
-// md5_orig implementation courtesy of
-// https://stackoverflow.com/questions/32163848/how-to-convert-string-to-md5-hash-using-ios-swift
-// originally from
-// http://iosdeveloperzone.com/2014/10/03/using-commoncrypto-in-swift/
-// implicitly released under the CC by-SA license
-func md5_orig(_ string: String) -> String {
-    let context = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
-    var digest = Array<UInt8>(repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-    CC_MD5_Init(context)
-    CC_MD5_Update(context, string, CC_LONG(string.lengthOfBytes(using: String.Encoding.utf8)))
-    CC_MD5_Final(&digest, context)
-    context.deallocate()
-    var hexString = ""
-    for byte in digest {
-        hexString += String(format:"%02x", byte)
-    }
-    return hexString
-}
-
 import CryptoKit
 
 // md5_new implementation courtesy of
@@ -31,9 +12,5 @@ func md5_new(_ string: String) -> String {
 }
 
 func md5(_ string: String) -> String {
-    if #available(iOS 13.0, *) {
-        return md5_new(string)
-    } else {
-        return md5_orig(string)
-    }
+    return md5_new(string)
 }
