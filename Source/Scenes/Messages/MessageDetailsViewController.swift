ccf7c9988d81e3ec183458342acf3cbcb767c261
kenstir <kenstir@gmail.com>
2025-11-26 10:58:33 -0500

    fix: a11y issue in Messages
    * Add accessibilityLabel to bar button item

--- Source/Scenes/Messages/MessageDetailsViewController.swift
+++ Source/Scenes/Messages/MessageDetailsViewController.swift
@@ -1,20 +1,18 @@
-/*
- * Copyright (c) 2023 Kenneth H. Cox
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+//
+//  Copyright (c) 2025 Kenneth H. Cox
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, see <https://www.gnu.org/licenses/>.
 
 import UIKit
 
@@ -45,6 +43,7 @@ class MessageDetailsViewController: UIViewController {
         self.setupHomeButton()
         let image = loadAssetImage(named: "mark_email_unread")
         let button = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(markUnreadButtonPressed(sender:)))
+        button.accessibilityLabel = "Mark message unread"
         navigationItem.rightBarButtonItems?.append(button)
 
         titleLabel.text = message?.title
