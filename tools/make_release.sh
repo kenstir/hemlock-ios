9a9edd984d48d3369be9dbc9c12283863c26cdfa
kenstir <kenstir@gmail.com>
2025-12-04 16:48:43 -0500

    feat: Automate build steps partially with fastlane (#73)
    * Use 'fastlane' only for build version maintenance.
      The code signing involved too much fastlane magic, and that seemed
      to me less useful than the magic done by xcode signing. At least xcode
      is maintained by Apple and has always worked.
    * Specify MARKETING_VERSION=4.2.0 CURRENT_PROJECT_VERSION=3 at the
      project level not the target level.  This will be easier in the
      long run and fastlane did not work without it.
    * Rewrite recent release notes a la thegooddocsproject.dev
    * Remove obsolete docs/
    * Update make_release.sh

---
+++ tools/make_release.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 app_name"
+    exit 1
+fi
+app="$1"
+
+proj="Hemlock.xcodeproj/project.pbxproj"
+
+set -e
+
+# determine scheme name
+# TODO: align scheme names with app names
+scheme=
+case "$app" in
+indiana)  scheme=Indiana;;
+mo)       scheme=Missouri;;
+noble)    scheme=NOBLE;;
+pines)    scheme=PINES;;
+sagecat)  scheme=SageCat;;
+owwl)     scheme=OWWL;;
+*)        scheme="$app";;
+esac
+
+# collect other metadata from Secrets/
+
+teamid=$(cat Secrets/teamid.kenstir)
+if [ -r Secrets/teamid.$app ]; then
+    teamid=$(cat Secrets/teamid.$app)
+fi
+username=$(cat Secrets/transport.username)
+password=$(cat Secrets/transport.password)
+
+# scrape version strings
+
+version=$(tools/fl print_build_info | awk '/version:/ {print $NF}')
+build=$(tools/fl print_build_info | awk '/build:/ {print $NF}')
+
+tag=${app}_${version}.${build}
+msg="${tag}"
+
+# trust but verify
+
+echo "vers:     $version"
+echo "build:    $build"
+echo "tag:      $tag"
+echo "teamid:   $teamid"
+echo "username: $username"
+test -n "$build"
+test -n "$version"
+test -n "$tag"
+test -n "$teamid"
+test -n "$username"
+test -n "$password"
+
+echo ""
+read -p "Continue ?" ans
+
+# build .xcarchive
+
+xcodebuild archive \
+  -scheme "$scheme" \
+  -configuration Release \
+  -destination generic/platform=iOS \
+  -archivePath build/$app.xcarchive
+
+# build .ipa
+
+xcodebuild -exportArchive \
+           -archivePath build/$app.xcarchive \
+           -exportOptionsPlist tools/ExportOptions.plist \
+           -exportPath build/export
+
+# upload it
+
+/Applications/Transporter.app/Contents/itms/bin/iTMSTransporter \
+    -m upload \
+    -assetFile build/export/$scheme.ipa \
+    -s "$teamid" \
+    -u "$username" \
+    -p "$password" \
+    -v eXtreme
+
+# tag it
+
+git commit "$proj" -m "$msg" || true
+git tag -a -m "$msg" $tag
+git push
+git push origin $tag
