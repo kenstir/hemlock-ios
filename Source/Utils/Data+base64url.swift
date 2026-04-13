064e040e53661ba862d02682d313cf79f2f14566
kenstir <kenstir@gmail.com>
2026-04-13 17:51:32 -0400

    Add String base64url extensions and a test

--- Source/Utils/Data+base64url.swift
+++ Source/Utils/Data+base64url.swift
@@ -23,7 +23,7 @@ SOFTWARE.
 */
 
 public extension Data {
-    public init?(base64urlEncoded input: String) {
+    init?(base64urlEncoded input: String) {
         var base64 = input
         base64 = base64.replacingOccurrences(of: "-", with: "+")
         base64 = base64.replacingOccurrences(of: "_", with: "/")
@@ -33,7 +33,7 @@ public extension Data {
         self.init(base64Encoded: base64)
     }
 
-    public func base64urlEncodedString() -> String {
+    func base64urlEncodedString() -> String {
         var result = self.base64EncodedString()
         result = result.replacingOccurrences(of: "+", with: "-")
         result = result.replacingOccurrences(of: "/", with: "_")
